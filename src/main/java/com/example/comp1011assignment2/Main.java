940715ffe8f06bfffa30e0e786e920557d3f40f1
JamesL2004 <James002>
2023-11-27 09:35:18 -0500

    Started the search view and added the icon image in the top corner.

--- src/main/java/com/example/comp1011assignment2/Main.java
+++ src/main/java/com/example/comp1011assignment2/Main.java
@@ -3,6 +3,7 @@ package com.example.comp1011assignment2;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,23 +11,24 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("SearchMangaView.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Search for Your Favourite Manga");
+        stage.getIcons().add(new Image(Main.class.getResourceAsStream("imgs/icon.png")));
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args)
     {
-        try {
+      /*  try {
             APIResponse response = APIUtility.callAPI("Demon Slayer");
             System.out.println(response.getResponse());
         } catch (IOException e) {
             throw new RuntimeException(e);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
-        }
-        //launch();
+        }*/
+        launch();
     }
 }
